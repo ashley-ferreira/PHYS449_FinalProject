069e3cb3e31b957a54a9ce709072eae9658af103
ashley-ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2022-10-30 23:57:33 -0400

    padding set back to default of none

--- networks/alex_net.py
+++ networks/alex_net.py
@@ -6,10 +6,11 @@ def Alex(input_shape, unique_labels=2, dropout_rate=0.5):
     '''
     Defines the 2D Convolutional Neural Network (CNN)
     Parameters:    
+
         input_shape (arr): input shape for network
-        training_labels (arr): training labels
         unique_labels (int): number unique labels 
         dropout_rate (float): dropout rate as fraction
+        
     Returns:
         
         model (keras model class): convolutional neural network to train
@@ -17,16 +18,16 @@ def Alex(input_shape, unique_labels=2, dropout_rate=0.5):
 
     model = Sequential()
 
-    model.add(Conv2D(filters=96, input_shape=input_shape, activation='relu', padding='same', kernel_size=(11,11)))
-    model.add(MaxPool2D(pool_size=(3,3), padding='valid'))
+    model.add(Conv2D(filters=96, input_shape=input_shape, activation='relu', kernel_size=(11,11)))
+    model.add(MaxPool2D(pool_size=(3,3)))
 
-    model.add(Conv2D(filters=256, input_shape=input_shape, activation='relu', padding='same', kernel_size=(5,5)))
-    model.add(MaxPool2D(pool_size=(3,3), padding='valid'))
+    model.add(Conv2D(filters=256, input_shape=input_shape, activation='relu', kernel_size=(5,5)))
+    model.add(MaxPool2D(pool_size=(3,3)))
 
-    model.add(Conv2D(filters=384, input_shape=input_shape, activation='relu', padding='same', kernel_size=(3,3)))
-    model.add(Conv2D(filters=384, input_shape=input_shape, activation='relu', padding='same', kernel_size=(3,3)))
-    model.add(Conv2D(filters=256, input_shape=input_shape, activation='relu', padding='same', kernel_size=(3,3)))
-    model.add(MaxPool2D(pool_size=(3,3), padding='valid'))
+    model.add(Conv2D(filters=384, input_shape=input_shape, activation='relu', kernel_size=(3,3)))
+    model.add(Conv2D(filters=384, input_shape=input_shape, activation='relu', kernel_size=(3,3)))
+    model.add(Conv2D(filters=256, input_shape=input_shape, activation='relu', kernel_size=(3,3)))
+    model.add(MaxPool2D(pool_size=(3,3)))
 
     model.add(Flatten())
     model.add(Dropout(dropout_rate))
--- networks/dieleman_net.py
+++ networks/dieleman_net.py
@@ -6,10 +6,11 @@ def Diel(input_shape, unique_labels=2, dropout_rate=0.5):
     '''
     Defines the 2D Convolutional Neural Network (CNN)
     Parameters:    
+
         input_shape (arr): input shape for network
-        training_labels (arr): training labels
         unique_labels (int): number unique labels 
         dropout_rate (float): dropout rate as fraction
+        
     Returns:
         
         model (keras model class): convolutional neural network to train
@@ -17,17 +18,17 @@ def Diel(input_shape, unique_labels=2, dropout_rate=0.5):
 
     model = Sequential()
 
-    model.add(Conv2D(filters=32, input_shape=input_shape, activation='relu', padding='same', kernel_size=(6,6)))
-    model.add(MaxPool2D(pool_size=(2, 2), padding='valid'))
+    model.add(Conv2D(filters=32, input_shape=input_shape, activation='relu', kernel_size=(6,6)))
+    model.add(MaxPool2D(pool_size=(2, 2)))
 
     model.add(BatchNormalization())
 
-    model.add(Conv2D(filters=64, input_shape=input_shape, activation='relu', padding='same', kernel_size=(5,5)))
-    model.add(MaxPool2D(pool_size=(2, 2), padding='valid'))
+    model.add(Conv2D(filters=64, input_shape=input_shape, activation='relu', kernel_size=(5,5)))
+    model.add(MaxPool2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(filters=128, input_shape=input_shape, activation='relu', padding='same', kernel_size=(3,3)))
-    model.add(Conv2D(filters=128, input_shape=input_shape, activation='relu', padding='same', kernel_size=(3,3)))
-    model.add(MaxPool2D(pool_size=(2, 2), padding='valid'))
+    model.add(Conv2D(filters=128, input_shape=input_shape, activation='relu', kernel_size=(3,3)))
+    model.add(Conv2D(filters=128, input_shape=input_shape, activation='relu', kernel_size=(3,3)))
+    model.add(MaxPool2D(pool_size=(2, 2)))
 
     model.add(Flatten())
     model.add(Dropout(dropout_rate))
